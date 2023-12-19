b4cbf03903d271e211c0e8a42485e727d4f4c791
Anatolii <anatolii.mart6@gmail.com>
2023-12-19 14:20:17 +0100

    NEW

---
+++ ASP_PR_GuestsBook/GuestsBook/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GuestsBook.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
