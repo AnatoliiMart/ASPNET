50e3c08e11bc9d4cf90339c612d1fd22fab6fba7
Anatolii <anatolii.mart6@gmail.com>
2024-01-17 00:27:25 +0200

    >>>

--- MusicPortal/DAL/Entities/UsersToConfirm.cs
+++ MusicPortal/DAL/Entities/UsersToConfirm.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HearMe.DAL.Entities
 {
-    internal class UsersToConfirm
+    [Table("tblUsersToConfirm")]
+    public class UsersToConfirm
     {
+        [Key]
+        public int Id { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? Login { get; set; }
+
+        public string? Password { get; set; }
+
+        public string? Salt { get; set; }
     }
 }
